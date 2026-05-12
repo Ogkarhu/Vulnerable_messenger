6d0e0561fca468bf18106ab0550e36aa7a361db4
Otso <otso.tilus@helsinki.fi>
2026-05-12 14:55:15 +0300

    database created, readme added

--- vulnerable_messenger/messenger/models.py
+++ vulnerable_messenger/messenger/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+    
+    
+class User(models.Model):
+    name = models.CharField()
+    user_id = models.IntegerField()
+    isadmin = models.BooleanField(default=False)
+    
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipient = models.IntegerField()
+    msg_id = models.IntegerField()
+    message = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    
