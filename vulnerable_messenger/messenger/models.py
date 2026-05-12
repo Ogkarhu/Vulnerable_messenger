bf4458c948f4c6a17b56506a6e334b954a60567e
Otso <otso.tilus@helsinki.fi>
2026-05-12 16:03:01 +0300

    adminhaavoittuvuus lisätty

--- vulnerable_messenger/messenger/models.py
+++ vulnerable_messenger/messenger/models.py
@@ -3,7 +3,7 @@ from django.db import models
     
     
 class User(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=30)
     user_id = models.IntegerField()
     isadmin = models.BooleanField(default=False)
     
--- vulnerable_messenger/messenger/views.py
+++ vulnerable_messenger/messenger/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import User
 
 
 def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        user_id = request.POST.get('user_id')
+        #this shouldn't be here
+        isadmin = request.POST.get('isadmin') == 'on'
+
+        if name and user_id:
+            User.objects.create(name=name, user_id=int(user_id), isadmin=isadmin)
+            return redirect('index')
     
-    return HttpResponse("Hello, world. You're at the polls index.")
+    users = User.objects.all()
+    return render(request, 'messenger/index.html', {'users': users})
+
+def messages(request, user_id):
+    return HttpResponse("Here be the messages that you should see")
+
+
